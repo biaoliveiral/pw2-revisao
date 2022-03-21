2f02753d334f2e1a3f0042394079837edcd9c7b9
joao-beatriz <joao-beatriz@test.com>
2022-03-21 11:00:25 -0300

    Desenvolvimento do exercicio 4

---
+++ src/ExercicioQuatro.java
@@ -0,0 +1,14 @@
+
+public class ExercicioQuatro {
+	
+	public static void main(String[] args) {
+		int n1 = 4;
+		
+		if(n1 > 0 && n1 != 15 && n1 % 2 == 0 && n1 % 3 == 0) {
+			System.out.println("O número atríbuido é um número da sorte!!!!");
+		}else {
+			System.out.println("O número atríbuido não é um número da sorte :(");
+		}
+	}
+
+}
